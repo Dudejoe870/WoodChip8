8c644717aac5fc1a5b73099eb3e1ae2431863f4c
John Clemis <dudejoe870@gmail.com>
2018-07-22 15:31:52 -0500

    Add project files.

---
+++ WoodChip8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WoodChip8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
